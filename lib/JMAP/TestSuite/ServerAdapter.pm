058087289829db694d44c71fb53edf4ea93bc872
Ricardo Signes <rjbs@cpan.org>
2018-07-15 13:57:31 -0400

    unshared accounts: can be pristine or can be "any" with a lock

--- lib/JMAP/TestSuite/ServerAdapter.pm
+++ lib/JMAP/TestSuite/ServerAdapter.pm
@@ -1,9 +1,35 @@
 package JMAP::TestSuite::ServerAdapter;
 use Moose::Role;
 
+use Hash::Util::FieldHash qw(fieldhash);
 use JMAP::Tester;
+use Scope::Guard ();
 
 requires 'any_account';
+
+has _locks => (
+  is    => 'rw',
+  lazy  => 1,
+  init_arg  => undef,
+  default   => sub {
+    fieldhash my %hash;
+    return \%hash;
+  },
+);
+
+sub unshared_account {
+  my ($self) = @_;
+  return $self->pristine_account if $self->can('pristine_account');
+
+  my $account 	= $self->any_account;
+
+  my $accountId = $account->accountId;
+  my $guard = Scope::Guard->new(sub { warn "releasing lock for $accountId" });
+
+  $self->_locks->{ $account } = $guard;
+
+  return $account;
+}
 
 no Moose::Role;
 1;
